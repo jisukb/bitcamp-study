c0d45b37eced57458ebfc3e94d66b48c74fae43a
jisukb <wwrbrbww@gmail.com>
2021-01-13 19:55:11 +0900

    .

--- hello2.cpp
+++ hello2.cpp
@@ -4,12 +4,12 @@ void print(int a) {
 	printf("==> %d\n", a);
 }
 
-void print2(float f) {
+void print(float f) {
 	printf("==> %f\n", f);
 }
 
 int main(int argc, char* argv[]) {
     printf("Hello!\n");
     print(100);
-    print2(3.14f);
+    print(3.14f);
 }
